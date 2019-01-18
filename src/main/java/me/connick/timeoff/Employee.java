d0b216ea8c5f28d6a44aa7530c295c2d9baf2df0
Connick Shields <connickshields@gmail.com>
2019-01-17 23:01:06 -0800

    added requests hashmap, also added request rest handling

--- src/main/java/me/connick/timeoff/Employee.java
+++ src/main/java/me/connick/timeoff/Employee.java
@@ -1,17 +1,23 @@
 package me.connick.timeoff;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.data.annotation.Id;
 
 public class Employee {
 
   @Id
   private String employeeId; // internal ID for mongo use
-  private int hours;
+  private int hours; // number of available hours of PTO
+  private Map<String, Integer> requests; // map of all past, present and future PTO
 
   public Employee() {}
 
   public Employee(int hours) {
     this.hours = hours;
+    this.requests = new HashMap<String, Integer>();
   }
 
   public String getId() {
@@ -26,7 +32,22 @@ public class Employee {
     this.hours = newHours;
   }
 
-  public void subtractHours(int amount) {
-    this.hours -= amount;
+  public Map<String, Integer> getRequests(){
+    return this.requests;
+  }
+
+  // takes a number of hours, and a date, and returns true if successful or false otherwise
+  public boolean timeoffRequest(int numHours, int year, int month, int day) throws DateTimeException{
+    if(numHours > this.hours){
+      return false;
+    }
+    String date = LocalDate.of(year, month, day).toString();
+    this.hours -= numHours;
+    // putIfAbsent returns null if key isn't already mapped, otherwise returns value already stored
+    if(this.requests.putIfAbsent(date, numHours) != null){
+      int check = this.requests.get(date);
+      this.requests.put(date, check+numHours);
+    }
+    return true;
   }
 }
--- src/main/java/me/connick/timeoff/TimeoffController.java
+++ src/main/java/me/connick/timeoff/TimeoffController.java
@@ -1,5 +1,7 @@
 package me.connick.timeoff;
 
+import java.util.Map;
+import java.time.DateTimeException;
 import java.util.List;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.net.URISyntaxException;
@@ -22,9 +24,9 @@ public class TimeoffController {
 	@Autowired
 	private EmployeeRepository repository;
 
-	// returns a JSON object of all employees
-	@RequestMapping(value = "/timeoff/employees", method=RequestMethod.POST, produces={"application/json"})
-	public ResponseEntity<Employee> create(@RequestParam(value="hours", defaultValue="0") int hours, HttpServletRequest request) throws URISyntaxException{
+	// handles POST for creating new employees
+	@RequestMapping(value = "/timeoff/employees/", method=RequestMethod.POST, produces={"application/json"})
+	public ResponseEntity<Employee> employeePost(@RequestParam(value="hours", defaultValue="0") int hours, HttpServletRequest request) throws URISyntaxException{
 		Employee newEmployee = new Employee(hours);
 		repository.save(newEmployee);
 		HttpHeaders respHead = new HttpHeaders();
@@ -34,15 +36,15 @@ public class TimeoffController {
 	}
 
 	// returns a JSON object of all employees
-	@RequestMapping(value = "/timeoff/employees", method=RequestMethod.GET, produces={"application/json"})
-	public ResponseEntity<List<Employee>> queryAll() {
+	@RequestMapping(value = "/timeoff/employees/", method=RequestMethod.GET, produces={"application/json"})
+	public ResponseEntity<List<Employee>> employeeGetAll() {
 		List<Employee> allEmployees = repository.findAll();
 		return new ResponseEntity<List<Employee>>(allEmployees, HttpStatus.OK);
 	}
 
 	// returns a JSON object of a specific employee
 	@RequestMapping(value = "/timeoff/employees/{id}", method=RequestMethod.GET, produces={"application/json"})
-	public ResponseEntity<Employee> query(@PathVariable("id") String id) throws EmployeeNotFoundException {
+	public ResponseEntity<Employee> employeeGet(@PathVariable("id") String id) throws EmployeeNotFoundException {
 		Employee e = repository.findByEmployeeId(id);
 		if(e == null){
 			throw new EmployeeNotFoundException();
@@ -51,15 +53,15 @@ public class TimeoffController {
 	}
 
 	// deletes all employees in the database
-	@RequestMapping(value = "/timeoff/employees", method=RequestMethod.DELETE)
-	public ResponseEntity<String> deleteAll() {
+	@RequestMapping(value = "/timeoff/employees/", method=RequestMethod.DELETE)
+	public ResponseEntity<String> employeeDeleteAll() {
 		repository.deleteAll();
 		return new ResponseEntity<String>(HttpStatus.OK);
 	}
 
 	// deletes a specific employee in the database
 	@RequestMapping(value = "/timeoff/employees/{id}", method=RequestMethod.DELETE)
-	public ResponseEntity<String> delete(@PathVariable("id") String id) throws EmployeeNotFoundException {
+	public ResponseEntity<String> employeeDelete(@PathVariable("id") String id) throws EmployeeNotFoundException {
 		Employee e = repository.findByEmployeeId(id);
 		if(e == null){
 			throw new EmployeeNotFoundException();
@@ -68,9 +70,63 @@ public class TimeoffController {
 		return new ResponseEntity<String>(HttpStatus.OK);
 	}
 
-	@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such Employee")  // 404
+	// handles POST for new PTO request
+	@RequestMapping(value = "/timeoff/requests/", method=RequestMethod.POST, produces={"application/json"})
+	public ResponseEntity<Employee> requestPost(@RequestParam(value="id") String id,
+																					@RequestParam(value="hours", defaultValue="0") int numHours,
+																					@RequestParam(value="year") int year,
+																					@RequestParam(value="month") int month,
+																					@RequestParam(value="day") int day,
+																					HttpServletRequest request)
+																					throws URISyntaxException, InvalidDateException, InvalidPTORequest{
+		Employee e = repository.findByEmployeeId(id);
+		if(e == null){
+			throw new EmployeeNotFoundException();
+		}
+		try {
+			boolean isValidPTO = e.timeoffRequest(numHours, year, month, day);
+			repository.save(e);
+			if(!isValidPTO){
+				throw new InvalidPTORequest();
+			}
+		} catch(DateTimeException exception) {
+			throw new InvalidDateException();
+		}
+
+		HttpHeaders respHead = new HttpHeaders();
+		URI resource = new URI(request.getRequestURL().toString()+e.getId());
+   	respHead.setLocation(resource);
+		return new ResponseEntity<Employee>(e, respHead, HttpStatus.CREATED);
+	}
+
+	// returns all scheduled PTO of a specific employee
+	@RequestMapping(value = "/timeoff/requests/{id}", method=RequestMethod.GET, produces={"application/json"})
+	public ResponseEntity<Map<String, Integer>> requestGet(@PathVariable("id") String id) throws EmployeeNotFoundException {
+		Employee e = repository.findByEmployeeId(id);
+		if(e == null){
+			throw new EmployeeNotFoundException();
+		}
+		return new ResponseEntity<Map<String, Integer>>(e.getRequests(), HttpStatus.OK);
+	}
+
+	// Custom Exception definitions for handling various errors
+
+	// handles a 404 response if a specific employee is not found
+	@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No Such Employee")
 	public class EmployeeNotFoundException extends RuntimeException {
-		// ...
+		private static final long serialVersionUID = 1L;
+	}
+
+	// handles a response if invalid date is supplied
+	@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Invalid Date")
+	public class InvalidDateException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+	}
+
+	// handles a response if invalid date is supplied
+	@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Not Enough Hours")
+	public class InvalidPTORequest extends RuntimeException {
+		private static final long serialVersionUID = 1L;
 	}
 
 }
